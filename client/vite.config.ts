043eb1f188491d94d2fde9f43aeddb60333ff156
Ezz <amroelghitany@gmail.com>
2025-07-29 01:18:15 +0300

    final adjustments for vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,22 +6,20 @@ import { defineConfig } from "vite";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig(({}) => {
-
-    return {
-        plugins: [
-            react(),
-           
-        ],
-        resolve: {
-            alias: {
-                "@": resolve(__dirname, "src"),
-                "@components": resolve(__dirname, "src/components"),
-                "@hooks": resolve(__dirname, "src/hooks"),
-                "@lib": resolve(__dirname, "src/lib"),
-                "@services": resolve(__dirname, "src/services"),
-                "@types": resolve(__dirname, "src/types"),
-            },
-        },
-    };
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+        "@components": resolve(__dirname, "src/components"),
+        "@hooks": resolve(__dirname, "src/hooks"),
+        "@lib": resolve(__dirname, "src/lib"),
+        "@services": resolve(__dirname, "src/services"),
+        "@types": resolve(__dirname, "src/types"),
+      },
+    },
+  };
 });
