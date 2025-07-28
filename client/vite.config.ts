2166a030820a9becce622657d40fe789a011c5e6
Ezz <amroelghitany@gmail.com>
2025-07-29 00:09:46 +0300

    Fix Config Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({}) => {
 
     return {
         plugins: [
